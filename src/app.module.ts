b31d45b6a9d05236b4dbda063d99a4951f811bea
Jair Lenin Lucas Benavides <jairlucasbe@gmail.com>
2024-11-14 17:16:31 -0500

    app.module.ts modificado

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClienteModule } from './clientes/clientes.module';
 
-@Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      url: 'mongodb+srv://grupoupgradeperu:CUhQCqGo3DG7z4sN@upgradedb.vcn6i.mongodb.net/campania',
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      database: 'campania',
-      synchronize: true,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    }),
-    ClienteModule
-  ],
-})
-export class AppModule {}
+  @Module({
+    imports: [
+      TypeOrmModule.forRoot({
+        type: 'mongodb',
+        url: 'mongodb+srv://grupoupgradeperu:CUhQCqGo3DG7z4sN@upgradedb.vcn6i.mongodb.net/campania',
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        database: 'campania',
+        synchronize: true,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      }),
+      ClienteModule
+    ],
+  })
+  export class AppModule {}
